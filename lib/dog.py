5f18a332902e0973d1edf24d428479e04054d0b9
Isma1Huz <ismael.hussein@student.moringaschool.com>
2023-09-11 08:30:54 +0300

    Done

--- lib/dog.py
+++ lib/dog.py
@@ -1,22 +1,43 @@
-from models import Dog
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
-def create_table(base):
-    pass
+Base = declarative_base()
 
-def save(session, dog):
-    pass
+class Dog(Base):
+    __tablename__ = 'dogs'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    breed = Column(String)
 
-def get_all(session):
-    pass
+    def __init__(self, name, breed):
+        self.name = name
+        self.breed = breed
 
-def find_by_name(session, name):
-    pass
+    def save(self):
+        Session = sessionmaker()
+        session = Session()
+        session.add(self)
+        session.commit()
+        session.close()
 
-def find_by_id(session, id):
-    pass
+    @classmethod
+    def create_table(cls):
+        engine = create_engine('sqlite:///dogs.db')
+        Base.metadata.create_all(engine)
 
-def find_by_name_and_breed(session, name, breed):
-    pass
+    @classmethod
+    def new_from_db(cls, row):
+        return cls(**dict(row))
 
-def update_breed(session, dog, breed):
-    pass
+    @classmethod
+    def get_all(cls):
+        return [cls.new_from_db(row) for row in cls.query.all()]
+
+    @classmethod
+    def find_by_name(cls, name):
+        return cls.query.filter_by(name=name).first()
+
+    @classmethod
+    def find_by_id(cls, id):
+        return cls.query.get(id)
